8949a5cfc2a61e43e53eadfa03b443c13e1b60ba
黄朝昆 <422823333@qq.com>
2024-08-27 23:14:48 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["huangchaokun.com"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["world--speed.com"];
 pub const PUBLIC_RS_PUB_KEY: &str = "COuU5JBSzo9GXU2O965rVdiMjDodjJjNwwFswwM4IB4=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
